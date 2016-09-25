feca095aa96ec42a58587556f288919d34aedce7
Bang Nguyen <bangnv77@yahoo.com>
2016-09-24 18:46:34 -0700

    change column name due to typo and add styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924221623) do
+ActiveRecord::Schema.define(version: 20160925014346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
-    t.text     "desciption"
+    t.text     "description"
     t.string   "author"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
